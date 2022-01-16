fdee63aae16a67e72a1cbaabc08b938b77f8d13e
Rumiko Ueno <momotarouko@gmail.com>
2022-01-16 15:07:46 +0900

    171. ユーザ入力の取得

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-// ここにコードを書きます
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+function searchAddressHandler(event: Event) {
+  // イベントに対するデフォルトの動作をキャンセルする
+  event.preventDefault();
+  const enterdAddress = addressInput.value;
+
+  // Google API に送信
+}
+
+form.addEventListener('submit', searchAddressHandler);
