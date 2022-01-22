3b4dbfb4ded463e0efb66aa8f2e125c838f2e319
Rumiko Ueno <momotarouko@gmail.com>
2022-01-22 09:28:41 +0900

    173. Axios & 住所の座標取得

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,36 @@
-const form = document.querySelector('form')!;
-const addressInput = document.getElementById('address')! as HTMLInputElement;
+import axios from "axios";
+
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const GOOGLE_API_KEY = "dmy";
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
 
 function searchAddressHandler(event: Event) {
   // イベントに対するデフォルトの動作をキャンセルする
   event.preventDefault();
-  const enterdAddress = addressInput.value;
+  const enteredAddress = addressInput.value;
 
-  // Google API に送信
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("座標を取得できませんでした。");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
 }
 
-form.addEventListener('submit', searchAddressHandler);
+form.addEventListener("submit", searchAddressHandler);
